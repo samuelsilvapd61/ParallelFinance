8e0ac0cf546f71b49d9e7d8db6f743918d68a7f6
Samuel de Oliveira Silva <samuelsilvapd61@gmail.com>
2025-03-03 23:21:22 -0300

    Initial commit for ParallelFinanceSingleThread

---
+++ parallelfinance/src/main/java/silva/oliveira/samuel/parallelfinance/ParallelFinanceSingleThreadApplication.java
@@ -0,0 +1,13 @@
+package silva.oliveira.samuel.parallelfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelFinanceSingleThreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelFinanceSingleThreadApplication.class, args);
+	}
+
+}
---
+++ parallelfinance/src/test/java/silva/oliveira/samuel/parallelfinance/ParallelFinanceSingleThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.oliveira.samuel.parallelfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelFinanceSingleThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
